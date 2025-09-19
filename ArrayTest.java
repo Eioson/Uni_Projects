8b82a01fa680cd705b3554137a58452189875ccc
Eioson <eiosonp@gmail.com>
2025-09-19 12:39:32 +0800

    [9/19 @ 12:38] Added a new program that presents a multiplication table using some math and a two dimensional array. Credits to geeks2geeks for the backbone of the code.

---
+++ ArrayTest.java
@@ -0,0 +1,23 @@
+
+public class ArrayTest {
+    public static void main(String[] args) {
+        
+        int n=10, m=10;
+        
+        int[][] arr= new int[n][m];
+
+
+        for (int i=1; i<m; i++){
+            for (int j=1; j<n; j++){
+                arr[i][j]=i*j;
+            }
+        }
+
+        for (int i = 1; i<n; i++){
+            for (int j = 1; j<m; j++){
+                System.out.printf("%3d", arr[i][j]); // Credits to Less
+            }
+            System.out.println();
+        }
+    }
+}
